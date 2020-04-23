62128a35ce54d45750de08ea576db2a092cab101
Jonathan T Dean <jonathantomdean@gmail.com>
2020-04-23 14:48:03 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -21,7 +21,8 @@ def third_challenge
         age:
         },
       :matriarch => {
-        #your key/value pairs here
+        name: ,
+        age:
         },
       :heroine => {
         #your key/value pairs here
